37539d28b1a5546a06a797156564b14e29ded49e
wycats <wycats@gmail.com>
2011-06-07 16:40:49 -0700

    Added feature for signing in via the Devise-provided form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,7 +2,9 @@ ActiveSupport::SecureRandom = SecureRandom
 
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.confirm! unless unconfirmed
   end
 end
 
